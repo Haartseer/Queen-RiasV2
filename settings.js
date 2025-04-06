86cf29f70ce3f04df6e1159ff6edd990ba9b828e
Haartseer <adeen6254@gmail.com>
2025-04-06 16:24:36 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,7 +14,7 @@ global.ownername = '𝐓𝐨𝐱𝐱𝐢𝐜 𝐁𝐨𝐲' //ur owner name
 
 // Information
 global.botname = '𝑸𝒖𝒆𝒆𝒏 𝑹𝒊𝒂𝒔 𝑽𝟐' //ur bot name
-global.ownernumber = '2348165846414' //ur owner number
+global.ownernumber = '27635326981' //ur owner number
 global.premium = ["2348165846414"] // Premium User
 /*--------------------------------------------*/
 // Type Choose true or false
